5df8cd2ce46754f73cb5a2d862cde0e4395b4e31
vickydarlinn <uttapalsangwan@gmail.com>
2023-08-03 11:11:17 +0530

    added copy to clipboard functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const colorBtn = document.querySelector(".btn");
 const seedColor = document.querySelector(".seed-color");
 const colorPalletes = document.querySelector(".color-palletes");
 const select = document.querySelector("#color-scheme");
+const output = document.querySelector(".output");
 
 colorBtn.addEventListener("click", function () {
   let hex = seedColor.value.slice(1);
@@ -13,18 +14,19 @@ colorBtn.addEventListener("click", function () {
     .then((res) => res.json())
     .then((data) => {
       console.log(data.colors);
+      colorPalletes.innerHTML = ""; // Clear the color palettes container
       data.colors.forEach((color) => {
         console.log(color.hex.value);
-        colorPalletes.innerHTML += `<div class="color" style="background-color: ${color.hex.value}"> 
-        <p> ${color.hex.value}
-        </div>`;
+        const colorElement = document.createElement("div");
+        colorElement.classList.add("color");
+        colorElement.style.backgroundColor = color.hex.value;
+        colorElement.innerHTML = `<p>${color.hex.value}</p>`;
+        colorPalletes.appendChild(colorElement);
       });
     });
-  colorPalletes.innerHTML = "";
 });
 
-// theme convertor
-
+// theme converter
 document.querySelector(".dark").addEventListener("click", function () {
   let root = document.querySelector(":root");
   root.style.setProperty("--light", " rgba(67, 65, 65, 0.85)");
@@ -32,6 +34,7 @@ document.querySelector(".dark").addEventListener("click", function () {
   document.querySelector(".dark").classList.add("hidden");
   document.querySelector(".light").classList.remove("hidden");
 });
+
 document.querySelector(".light").addEventListener("click", function () {
   let root = document.querySelector(":root");
   root.style.setProperty("--light", "rgba(228, 228, 228, 0.635)");
@@ -39,3 +42,33 @@ document.querySelector(".light").addEventListener("click", function () {
   document.querySelector(".dark").classList.remove("hidden");
   document.querySelector(".light").classList.add("hidden");
 });
+
+// Add copy functionality to the color boxes using event delegation
+colorPalletes.addEventListener("click", function (event) {
+  if (event.target.classList.contains("color")) {
+    const colorValue = event.target.textContent.trim();
+    console.log(colorValue);
+    copyToClipboard(colorValue);
+  }
+});
+
+// Function to copy text to clipboard
+function copyToClipboard(text) {
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      showOutput();
+      setTimeout(() => {
+        closeOutput();
+      }, 3000);
+    })
+    .catch((err) => {
+      console.error("Failed to copy: ", err);
+    });
+}
+function showOutput() {
+  output.style.display = "block";
+}
+function closeOutput() {
+  output.style.display = "none";
+}
